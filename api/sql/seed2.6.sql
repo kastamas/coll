273e78d9a8faa5789ad8f4073eaf1e7ac8e363f0
Michael <kastamas13@gmail.com>
2017-02-14 16:48:35 +0300

    version 2.6.0

---
+++ api/sql/seed2.6.sql
@@ -0,0 +1,30 @@
+--
+-- PostgreSQL database dump
+--
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET search_path = public, pg_catalog;
+
+
+-- ALter Types for ENUM
+ALTER TYPE enum_characteristic_substantive_case_type ADD VALUE 'vocative';--Для нового звательного падежа
+
+-- Запрос на добавление
+ALTER TYPE enum_characteristic_relation_to_main ADD VALUE 'distant_prepos';--Для нового звательного падежа
+
+-- Запрос на доп Enum
+ALTER TYPE enum_characteristic_relation_to_main ADD VALUE 'distant_postpos';
+
+-- Update --
+UPDATE collocations  SET characteristic_relation_to_main = 'distant_prepos' WHERE characteristic_relation_to_main = 'distant';
+
+--
+-- PostgreSQL database dump complete
+--
