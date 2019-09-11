29e3bab62ccfadbecf663106af370fe2b29b47e5
msidorov <mikxsid@gmail.com>
2019-09-11 04:38:38 +0300

    Upgrade

--- app/admin/admin.component.js
+++ app/admin/admin.component.js
@@ -4,16 +4,26 @@ angular
   .module("admin")
   .controller("AdminCtrl", [
     "$window",
-    function($window) {
+    "$route",
+    function($window, $route) {
       const ctrl = this;
+
+      ctrl.isAdmin = $window.localStorage.getItem("isAdmin");
 
       ctrl.checkCode = function() {
         if (ctrl.code === "only4Admin") {
           alert("Доступ подтвержден!");
           $window.localStorage.setItem("isAdmin", "true");
+          $route.reload();
         } else {
           alert("Код неверный!");
         }
+      };
+
+      ctrl.cancelSession = function() {
+        $window.localStorage.removeItem("isAdmin");
+        alert("Сессия завешена!");
+        $route.reload();
       };
     }
   ])
